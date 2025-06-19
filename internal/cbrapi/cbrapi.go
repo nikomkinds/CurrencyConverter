ab4fcdfa8c7f1871f9c0a74e8277e01ea00bde51
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-19 13:42:57 +0300

    implemented parsing structures

--- internal/cbrapi/cbrapi.go
+++ internal/cbrapi/cbrapi.go
@@ -1,6 +1,18 @@
 package cbrapi
 
-// TODO : ValCurs struct, Currency struct
+import "encoding/xml"
+
+// ValCurs structure for CBR XML parsing
+type ValCurs struct {
+	XMLName xml.Name   `xml:"ValCurs"` // XML root
+	Valute  []Currency `xml:"Valute"`  // list of currencies
+}
+
+// Currency structure for parsing each currency with its rate
+type Currency struct {
+	CharCode  string `xml:"CharCode"`
+	VunitRate string `xml:"VunitRate"`
+}
 
 // TODO : GetRates(dateReq string) (map[string]float64, error)
 
