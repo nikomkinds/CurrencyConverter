1b460c812d7c7f765555097866f497ec29081130
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-19 14:40:50 +0300

    implemented GetRates func

--- internal/cbrapi/cbrapi.go
+++ internal/cbrapi/cbrapi.go
@@ -2,6 +2,9 @@ package cbrapi
 
 import (
 	"encoding/xml"
+	"io"
+	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -9,7 +12,7 @@ import (
 // ValCurs structure for CBR XML parsing
 type ValCurs struct {
 	XMLName xml.Name   `xml:"ValCurs"` // XML root
-	Valute  []Currency `xml:"Valute"`  // list of currencies
+	Valutes []Currency `xml:"Valute"`  // list of currencies
 }
 
 // Currency structure for parsing each currency with its rate
@@ -18,7 +21,51 @@ type Currency struct {
 	VunitRate string `xml:"VunitRate"`
 }
 
-// TODO : GetRates(dateReq string) (map[string]float64, error)
+func GetRates(dateReq string) (map[string]float64, error) {
+
+	// base url for the last available date
+	url := "http://www.cbr.ru/scripts/XML_daily.asp"
+	// add date if provided
+	if dateReq != "" && isValidDate(dateReq) {
+		url += "?date_req=" + dateReq
+	}
+
+	// getting data from CBR
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal XML
+	var valCurs ValCurs
+	if err := xml.Unmarshal(body, &valCurs); err != nil {
+		return nil, err
+	}
+
+	// reading all currencies
+	rates := make(map[string]float64)
+	rates["RUB"] = 1.0
+	for _, v := range valCurs.Valutes {
+
+		value, err := parseRussianFloat(v.VunitRate)
+		if err != nil {
+			continue
+		}
+		rates[v.CharCode] = value
+	}
+	return rates, nil
+}
+
+func isValidDate(date string) bool {
+	re := regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})&`)
+	return re.MatchString(date)
+}
 
 func parseRussianFloat(num string) (float64, error) {
 
