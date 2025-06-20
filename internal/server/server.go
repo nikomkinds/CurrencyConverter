1b3c6b59d3a1e32f6df020036bf4f5805af2b5e7
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-20 13:47:52 +0300

    implemented server struct init

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,24 @@
 package server
 
-// TODO : Server struct, NewServer(), configureRoutes(), Run()
+import "github.com/gin-gonic/gin"
+
+type Server struct {
+	router *gin.Engine
+}
+
+func NewServer() *Server {
+	r := gin.Default()
+	s := &Server{router: r}
+	s.configureRoutes()
+	return s
+}
+
+func (s *Server) configureRoutes() {
+	s.router.GET("/convert", s.handleConversion)
+}
 
 // TODO : s.handleConversion
+
+func (s *Server) handleConversion(c *gin.Context) {
+
+}
