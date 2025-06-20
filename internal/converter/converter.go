3e2eeb8238bf0e8184a09c98e6b44b7848248a63
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-20 13:16:13 +0300

    implemented ConversionResult struct

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -1,5 +1,11 @@
 package converter
 
-// TODO : ConversionResult struct
+type ConversionResult struct {
+	From      string  `json:"from"`
+	To        string  `json:"to"`
+	Amount    float64 `json:"amount"`
+	Converted float64 `json:"converted"`
+	Rate      float64 `json:"rate"`
+}
 
 // TODO : Convert (from, to, amount string) (*ConversionResult, error)
