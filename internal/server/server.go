eebaa1d4bad6f90d4fe7360905c655834bd40dd1
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-06-20 21:09:09 +0300

    implemented conversion handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,10 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nikomkinds/CurrencyConverter/internal/converter"
+	"net/http"
+)
 
 type Server struct {
 	router *gin.Engine
@@ -17,8 +21,27 @@ func (s *Server) configureRoutes() {
 	s.router.GET("/convert", s.handleConversion)
 }
 
-// TODO : s.handleConversion
+func (s *Server) Run() {
+	s.router.Run(":8080")
+}
 
 func (s *Server) handleConversion(c *gin.Context) {
 
+	from := c.Query("from")
+	to := c.Query("to")
+	amount := c.Query("amount")
+	dateReq := c.Query("date_req")
+
+	if from == "" || to == "" || amount == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "all three params 'from', 'to' and 'amount' are required"})
+		return
+	}
+
+	result, err := converter.Convert(from, to, amount, dateReq)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"result": result})
 }
